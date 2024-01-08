9070134a254ea3c1300aa142fec0cd018d198b02
Nagi Reddy <nagi.reddy@buhlergroup.com>
2024-01-08 10:26:15 +0000

    MultiNavigation

--- webapp/controller/PODetail.controller.js
+++ webapp/controller/PODetail.controller.js
@@ -3,11 +3,11 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel"
 ], function (Controller, JSONModel) {
     "use strict";
-    return Controller.extend("MyApp.controller.POList", {
-        gotoDetail: function(){
+    return Controller.extend("MyApp.controller.PODetail", {
+        gotohome: function(){
       
             const router = this.getOwnerComponent().getRouter();
-            router.navTo("Detail");
+            router.navTo("HomeRoute");
 
         }
 
